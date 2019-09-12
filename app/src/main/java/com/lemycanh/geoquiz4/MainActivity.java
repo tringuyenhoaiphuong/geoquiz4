7fcf399e54d487afb046783a4def100b94ced9cf
Canh Le My <lemycanh@gmail.com>
2019-09-12 08:17:21 +0700

    !! Init Commit

---
+++ app/src/main/java/com/lemycanh/geoquiz4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lemycanh.geoquiz4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
